4aacaf1c010170ff9a2a82a397c324ad3e81dd94
Michael Kevin <michaelkmb@hotmail.com>
2023-08-22 19:12:13 -0300

    instant messenger encapsulation example;

---
+++ edu.example.encapsulation/src/edu/example/instant_messenger/ComputadorPedrinho.java
@@ -0,0 +1,11 @@
+package edu.example.instant_messenger;
+
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		MSNMessenger desktop = new MSNMessenger();
+		
+		desktop.enviarMensagem();
+		
+		desktop.receberMensagem();
+	}
+}
